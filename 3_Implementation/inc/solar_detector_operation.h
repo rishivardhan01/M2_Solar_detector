2875c26bf0d297b53f10c81cdec2b167a489cd88
rishivardhan01 <63336586+rishivardhan01@users.noreply.github.com>
2022-04-21 20:07:57 +0530

    Add files via upload

---
+++ 3_Implementation/inc/solar_detector_operation.h
@@ -0,0 +1,43 @@
+/**
+ * @file Solar_detector_operation.h
+ * @author RISHIVARDHAN T R (rishivardhan.tr.2018.ece@ritchennai.edu.in)
+ * @brief Header file for Solar_Detector are connected to AVR328 MCU GPIO pin
+ * @version 0.1
+ * @date 2022-04-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <Servo.h>
+
+int sensorPin = A0;
+int servoPin  = 9;
+
+int sensorValue = 0;
+int servoGrad = 90;
+int tolerance = 40;
+
+Servo myservo;
+
+void setup() {
+  pinMode( sensorPin, INPUT);
+  myservo.attach( servoPin );
+  myservo.write( servoGrad );
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  if ( sensorValue < (512-tolerance) )
+  {
+    if (servoGrad < 180) servoGrad++;
+  }
+
+  if ( sensorValue > (512+tolerance) )
+  {
+    if (servoGrad > 0) servoGrad--;
+  }
+
+  myservo.write( servoGrad ); 
+
+  delay(100);
+}
